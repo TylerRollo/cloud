09be4867a9ce750435e870b142cd07fd80a3a474
Tyler <trollo2013@gmail.com>
2025-02-28 23:52:33 -0500

    added public and private subnet

---
+++ network.tf
@@ -0,0 +1,24 @@
+locals {
+  vpc_cidr            = "10.0.0.0/16"
+  private_subnet_cidr = "10.0.0.0/24"
+  public_subnet_cidr  = "10.0.128.0/24"
+}
+
+data "aws_vpc" "default" {
+  default = false
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = local.vpc_cidr
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.private_subnet_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.public_subnet_cidr
+}
+
